b45c43d4bb8ca4a1bdc9b6b9d76b787c36293926
JaeCheolSim <simsimjae@naver.com>
2017-07-06 15:10:59 +0900

    10강 예제
    AOP 개념 2번째
    aspectj 문법을 사용한 pointcut 범위 지정(어노테이션 사용 ,xml사용)

---
+++ simjaecheol/Spring Lecture 10/src/main/java/com/javalec/ex/LogAop.java
@@ -0,0 +1,46 @@
+package com.javalec.ex;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class LogAop {
+	//@Pointcut("execution(public void get*(..))") public void인 모든 get메소드
+	//@Pointcut("execution(* com.javalec.ex.*.*())") com.javalec.ex. 패키지에 파라미터가 없는 모든 메소드
+	//@Pointcut("execution(* com.javalec.ex..*.*())") com.javalec.ex 패키지와 그 하위 패키지에 있는 파라미터가 없는 모든 메소드
+	//@Pointcut("execution(* com.javalec.ex.Worker.*())") com.javalec.ex.Worker 안의 모든 메소드
+	//@Pointcut("within(com.javalec.ex.*)")	com.javalec.ex 패키지 안에 있는 모든 메소드
+	//@Pointcut("within(com.javalec.ex..*)")com.javalec.ex 패키지 및 하위 패키지 안에 있는 모든 메소드
+	//@Pointcut("within(com.javalec.ex.Worker)")com.javalec.ex.Worker 모든 메소드
+	//@Pointcut("bean(student)") student 빈에만 적용
+	@Pointcut("bean(*ker)") //~ker로 끝나는 빈에만 적용W
+	private void pointcutMethod() {
+		}
+	
+	@Around("pointcutMethod()")
+	public Object loggerAop(ProceedingJoinPoint joinpoint) throws Throwable{
+		String signatureStr = joinpoint.getSignature().toShortString();
+		System.out.println(signatureStr + "is start");
+		long st = System.currentTimeMillis();
+		
+		try {
+			Object obj = joinpoint.proceed();
+			return obj;
+		}
+		finally {
+			long et = System.currentTimeMillis();
+			System.out.println(signatureStr + "is finished");
+			System.out.println("경과 시간" + (et-st));
+		}
+	}
+	
+	
+	@Before("bean(student)")
+	public void beforAdvice() {
+		System.out.println("beforAdvice()");
+	}
+	
+}
