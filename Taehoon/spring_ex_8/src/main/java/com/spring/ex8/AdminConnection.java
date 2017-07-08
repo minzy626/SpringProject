ecdcd467d93f5ac53636632fbce3122107ec0483
TaehoonJeong <anytime7739@naver.com>
2017-07-08 22:28:34 +0900

    8강
    8강

---
+++ Taehoon/spring_ex_8/src/main/java/com/spring/ex8/AdminConnection.java
@@ -0,0 +1,51 @@
+package com.spring.ex8;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+
+public class AdminConnection implements EnvironmentAware, InitializingBean, DisposableBean{
+
+	private Environment env;
+	private String adminId;
+	private String adminPw;
+	
+	@Override
+	public void setEnvironment(Environment env) {
+		System.out.println("setEnvironment()");
+		setEnv(env);
+	}
+	
+	public void setEnv(Environment env) {
+		this.env = env;
+	}
+
+	public String getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getAdminPw() {
+		return adminPw;
+	}
+
+	public void setAdminPw(String adminPw) {
+		this.adminPw = adminPw;
+	}
+	
+	@Override
+	public void afterPropertiesSet() throws Exception{
+		System.out.println("afterPropertiesSet()");
+		setAdminId(env.getProperty("admin.id"));
+		setAdminPw(env.getProperty("admin.pw"));
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("distroy()");
+	}
+}
