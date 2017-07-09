2580b527904fa6436f163effe5183e4a253993fa
NohDongGil <ndk1057@gmail.com>
2017-07-09 22:41:21 +0900

    7강

---
+++ DongGIl/Lecture7_3/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,21 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+		Student student1 = ctx.getBean("student",Student.class);
+		System.out.println(student1.getName());
+		System.out.println(student1.getAge());
+		Student student2 = ctx.getBean("student", Student.class);
+		student2.setName("김김김");
+		student2.setAge(34);
+		System.out.println(student2.getName());
+		System.out.println(student2.getAge());
+	}
+
+}
