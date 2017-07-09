d07e3f2efe5994e6799cde103fe506bc67e4f8eb
NohDongGil <ndk1057@gmail.com>
2017-07-09 22:36:21 +0900

    6강

---
+++ DongGIl/Lecture6_4/src/main/java/com/javalec/ex/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.javalec.ex;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:applicationCTX.xml")
+public class ApplicationConfig {
+	@Bean
+	public Student student1() {
+		
+		ArrayList<String> hobbys = new ArrayList<String>();
+		hobbys.add("잠");
+		hobbys.add("게임");
+		
+		Student student = new Student("노동길", 25, hobbys);
+		student.setHeight(180);
+		student.setWeight(90);
+		
+		return student;
+	}
+}
