1ac1b93ccfe23c622522f6c544e78eb64e3f9db8
NohDongGil <ndk1057@gmail.com>
2017-07-09 22:31:20 +0900

    5강

---
+++ DongGIl/Lecture5_2/src/main/java/com/javalec/ex/Pencil4B.java
@@ -0,0 +1,7 @@
+package com.javalec.ex;
+
+public class Pencil4B implements Pencil{
+	public void use() {
+		System.out.println("4B굵기로 쓰임");
+	}
+}
---
+++ DongGIl/Lecture5_2/src/main/java/com/javalec/ex/Pencil6BEraser.java
@@ -0,0 +1,7 @@
+package com.javalec.ex;
+
+public class Pencil6BEraser implements Pencil{
+	public void use() {
+		System.out.println("6B굵기로 쓰이고 지우개가 사용됨");
+	}
+}
