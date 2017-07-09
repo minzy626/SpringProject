2fb47c21ad5685cc7360d8f4c20d829203b46c49
NohDongGil <ndk1057@gmail.com>
2017-07-09 22:42:42 +0900

    8강

---
+++ DongGIl/Lecture8_2/src/main/java/com/javalec/ex/MainClass.java
@@ -0,0 +1,21 @@
+package com.javalec.ex;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AbstractApplicationContext ctx = new GenericXmlApplicationContext("classpath:applicationCTX.xml");
+		
+		AdminConnection connection = ctx.getBean("adminConnection", AdminConnection.class);
+		System.out.println("adminId : " + connection.getAdminId());
+		System.out.println("adminPw : " + connection.getAdminPw());
+		System.out.println("sub_adminId : " + connection.getSub_adminId());
+		System.out.println("sub_adminPw : " + connection.getSub_adminPw());
+		
+		ctx.close();
+	}
+
+}
