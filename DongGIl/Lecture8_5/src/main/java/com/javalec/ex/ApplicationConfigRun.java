566261e30b8bf8d3a7585018006701aed4ec9b07
NohDongGil <ndk1057@gmail.com>
2017-07-09 22:44:24 +0900

    8강

---
+++ DongGIl/Lecture8_5/src/main/java/com/javalec/ex/ApplicationConfigRun.java
@@ -0,0 +1,18 @@
+package com.javalec.ex;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("run")
+public class ApplicationConfigRun {
+	
+	@Bean
+	public ServerInfo serverInfo() {
+		ServerInfo info = new ServerInfo();
+		info.setIpNum("192.16.128.0");
+		info.setPortNum("80");
+		return info;
+	}
+}
