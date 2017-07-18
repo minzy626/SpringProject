aa475e7ef30484503605d823c6a7ece378732728
NohDongGil <ndk1057@gmail.com>
2017-07-19 00:42:20 +0900

    13강

---
+++ DongGIl/Lecture13/src/main/java/com/javalec/noh_ex13/Member.java
@@ -0,0 +1,33 @@
+package com.javalec.noh_ex13;
+
+public class Member {
+	private String name;
+	private String Id;
+	private String pw;
+	private String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return Id;
+	}
+	public void setId(String id) {
+		Id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
