d1590b6c639761813fec33b70c6515f9afa103eb
SimJaecheol <simsimjae@naver.com>
2017-08-08 08:38:48 +0900

    BController 주석제거

--- SPRING BOARD/Spring Board Project/src/main/java/com/javalec/ex/controller/BController.java
+++ SPRING BOARD/Spring Board Project/src/main/java/com/javalec/ex/controller/BController.java
@@ -28,8 +28,7 @@ public class BController {
 	public BController() {
 		
 	}
-	
-<<<<<<< HEAD
+
 	
 	/*@RequestMapping("/list") //占쌉쏙옙占쏙옙 占쏙옙 占쏙옙占� 占쏙옙占쏙옙占쏙옙占쏙옙
 	public String list(Model model) // Controller->Command->DAO占쏙옙 占쏙옙占식쇽옙 占쏙옙占쏙옙占싶몌옙 占쌕쏙옙 占쏙옙占쏙옙占쏙옙占실뤄옙 model占쏙옙체占쏙옙 占쏙옙占쌘뤄옙 占쌨아억옙占쏙옙. 
@@ -41,39 +40,20 @@ public class BController {
 
 	
 	
-	@RequestMapping(value="/write", method = RequestMethod.POST) //占쏙옙 占쌜쇽옙
-	public String write(BDto bDto,Model model) //占쏙옙 占쌜쇽옙占쏙옙 占쌜쇽옙占쏙옙,占쏙옙占쏙옙占� 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占싶야되니깍옙 request占쏙옙 占쏙옙占쌘로받아억옙占쏙옙.  
+	@RequestMapping(value="/write", method = RequestMethod.POST) 
+	public String write(BDto bDto,Model model)  
 	{
 		BWriteService command= new BWriteService();
 		command.execute(sqlsession,bDto);
-		return "redirect:/board/list"; //占쏙옙 占쌜쇽옙 占싹곤옙占쏙옙占쏙옙 占쌕쏙옙 占쏙옙 占쏙옙占쏙옙占� 占쏙옙占쏙옙占쏙옙 占싹므뤄옙 list.jsp호占쏙옙품途占�.
-=======
-	@RequestMapping(value="/write", method = RequestMethod.POST)
-	public String write(BDto bDto,Model model)
-	{
-		BWriteService command= new BWriteService();
-		command.execute(sqlsession,bDto);
-		return "redirect:list";
->>>>>>> 0deb6e1e6383add36e31f689112c1196cc272caf
+		return "redirect:/board/list"; 
 	}
+	
 	
 	
 	@RequestMapping("/write_view")
 	public String write_view(Model model) {
-<<<<<<< HEAD
-		return "write_view";} // 占쌜억옙占쏙옙 占쌨댐옙 占쏙옙占쏙옙占쏙옙占쏙옙 占쌕뤄옙 화占썽만 占쏙옙占쏙옙占쌍몌옙 占실니깍옙 占쌕뤄옙 占쏙옙占쏙옙占쏙옙占쌍몌옙 占쏙옙
-	
-	
-	
-	@RequestMapping("/content_view") //占쏙옙 占쏙옙占쏙옙 占쏙옙占쏙옙
-	public String content_view(HttpServletRequest request,Model model) { //list.jsp占쏙옙占쏙옙 占쏙옙 占쏙옙占� 占쌜몌옙占쏙옙 ID占쏙옙占쏙옙 占쌔댐옙占싹댐옙 占쏙옙占쏙옙 占쏙옙占� 占쏙옙占쏙옙占� 占싹므뤄옙 request 占십울옙
-		model.addAttribute("request",request);
-		command = new BContentService();
-		command.execute(sqlsession,model);  
-		return "content_view"; //占썰에 占싼뤄옙占쏙옙占쏙옙,model占쏙옙占쏙옙 占쏙옙占쏙옙 占폗占쏙옙占쏙옙 dto占쏙옙 占쏙옙占쏙옙占쏙옙占�. jsp占쏙옙占싹울옙占쏙옙 dto占쏙옙 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占�.
-=======
+
 		return "write_view";} 
-	
 	
 	
 	@RequestMapping("/content_view") 
@@ -81,30 +61,24 @@ public class BController {
 		model.addAttribute("request",request);
 		command = new BContentService();
 		command.execute(sqlsession,model);  
-		return "content_view";
->>>>>>> 0deb6e1e6383add36e31f689112c1196cc272caf
-	} 
+		return "content_view"; 
+
+	}
+
 	
-	
-	@RequestMapping(value="/modify") //占쏙옙 占쏙옙占쏙옙占싹깍옙
+	@RequestMapping(value="/modify")
 	public String modify(BDto bDto,Model model) { 
 		BModifyService command = new BModifyService();
 		command.execute(sqlsession,bDto);  
-<<<<<<< HEAD
-		return "redirect:/board/list"; //占쏙옙 占쏙옙占쏙옙占싼듸옙 占쌕쏙옙 占쏙옙 占쏙옙占� 占쏙옙占�
-	} 
-	//GET占쏙옙캅占� POST占쏙옙占쏙옙占� 占쏙옙占쏙옙  https://blog.outsider.ne.kr/312 
-	
-	@RequestMapping("/delete") //占쏙옙 占쏙옙占쏙옙占싹깍옙
-	public String delete(HttpServletRequest request,Model model) { //request占쏙옙占쏙옙 占쏙옙占쏙옙 id占쏙옙 占쏙옙占쏙옙占쏙옙占쏙옙占�. 
-=======
-		return "redirect:list";
-	} 
 
+		return "redirect:/board/list"; 
+	} 
+	
+	
+	
 	
 	@RequestMapping("/delete") //�� �����ϱ�
 	public String delete(HttpServletRequest request,Model model) { 
->>>>>>> 0deb6e1e6383add36e31f689112c1196cc272caf
 		model.addAttribute("request",request);
 		command = new BDeleteService(); 
 		command.execute(sqlsession,model);   
